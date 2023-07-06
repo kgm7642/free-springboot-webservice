1116fa7ceacf30e0295a7e557b692162f7271e53
KyungMin <kgm7642@gmail.com>
2023-07-07 06:19:41 +0900

    Initial commit

---
+++ src/main/java/com/book/FreeSpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeSpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeSpringbootWebserviceApplication.class, args);
+	}
+
+}
