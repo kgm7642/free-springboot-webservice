b77d35a7a9fddd7322df975d7227a8534177b6ec
KyungMin <kgm7642@gmail.com>
2023-07-15 21:15:33 +0900

    PostsApiControllerTest 1차 시도

---
+++ src/main/java/com/book/domain/posts/PostService.java
@@ -0,0 +1,18 @@
+package com.book.domain.posts;
+
+import com.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
