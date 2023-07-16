1c59bed6d4bf99b89a9dd027afccece68992bb87
KyungMin <kgm7642@gmail.com>
2023-07-16 14:28:49 +0900

    수정/조회 기능 테스트 해보기

--- src/main/java/com/book/web/PostsApiController.java
+++ src/main/java/com/book/web/PostsApiController.java
@@ -1,20 +1,30 @@
 package com.book.web;
 
-import com.book.domain.posts.PostService;
+import com.book.domain.posts.PostsService;
+import com.book.web.dto.PostsResponseDto;
 import com.book.web.dto.PostsSaveRequestDto;
+import com.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
 
-    private final PostService postService;
+    private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-        return postService.save(requestDto);
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
