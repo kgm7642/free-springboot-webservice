3d69dc1b058518262e85023e0c6e783082f7153d
KyungMin <kgm7642@gmail.com>
2023-07-24 23:30:58 +0900

    게시글 조회 만들기

--- src/main/java/com/book/domain/posts/PostsService.java
+++ src/main/java/com/book/domain/posts/PostsService.java
@@ -1,11 +1,15 @@
 package com.book.domain.posts;
 
+import com.book.web.dto.PostsListResponseDto;
 import com.book.web.dto.PostsResponseDto;
 import com.book.web.dto.PostsSaveRequestDto;
 import com.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
